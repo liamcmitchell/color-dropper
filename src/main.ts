c932e2b0a0ef5b40034a533f69fe3f177c46cbce
Liam Mitchell <git@liam.geek.nz>
2024-05-07 21:31:44 +0200

    Working?

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,86 @@
 import "./style.css";
+
+const dropper = document.getElementById("dropper") as HTMLButtonElement;
+const headerColor = document.getElementById("header-color") as HTMLDivElement;
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+const zoom = document.getElementById("zoom") as HTMLDivElement;
+const zoomColor = document.getElementById("zoom-color") as HTMLDivElement;
+const zoomSize = 160;
+
+let loaded = false;
+let dropping = false;
+let hovering = false;
+let mouseX = 0;
+let mouseY = 0;
+
+const update = () => {
+  if (dropping) {
+    document.documentElement.classList.add("dropping");
+  } else {
+    document.documentElement.classList.remove("dropping");
+  }
+  if (loaded && dropping && hovering) {
+    zoom.style.display = "flex";
+    zoom.style.top = `${mouseY - zoomSize / 2}px`;
+    zoom.style.left = `${mouseX - zoomSize / 2}px`;
+    const hex = getColor();
+    zoomColor.innerHTML = hex;
+    zoom.style.setProperty("--color", hex);
+  } else {
+    zoom.style.display = "none";
+  }
+};
+
+const getColor = () => {
+  const pixel = ctx.getImageData(
+    mouseX * (image.width / canvas.clientWidth),
+    mouseY * (image.height / canvas.clientHeight),
+    1,
+    1,
+  );
+  return (
+    "#" +
+    Array.from(pixel.data.slice(0, 3))
+      .map((channel) => channel.toString(16).padStart(2, "0"))
+      .join("")
+  );
+};
+
+dropper.addEventListener("click", () => {
+  dropping = !dropping;
+  update();
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  hovering = true;
+  mouseX = event.offsetX;
+  mouseY = event.offsetY;
+  update();
+});
+
+canvas.addEventListener("mouseout", () => {
+  hovering = false;
+  update();
+});
+
+canvas.addEventListener("click", () => {
+  if (loaded && dropping && hovering) {
+    headerColor.innerHTML = getColor();
+  }
+});
+
+const image = new Image();
+image.addEventListener("error", () => {
+  alert("Image load error");
+});
+image.addEventListener("load", () => {
+  canvas.width = image.width;
+  canvas.height = image.height;
+  canvas.style.aspectRatio = String(image.width / image.height);
+  ctx.drawImage(image, 0, 0);
+  loaded = true;
+  update();
+});
+image.src =
+  "/1920x1080-4598441-beach-water-pier-tropical-sky-sea-clouds-island-palm-trees.jpg";
