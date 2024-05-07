1213a08ddd18102f7f90e4a395b8d2ef4abf5ce9
Liam Mitchell <git@liam.geek.nz>
2024-05-07 21:52:20 +0200

    Allow uploading other files

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import "./style.css";
 
 const dropper = document.getElementById("dropper") as HTMLButtonElement;
 const headerColor = document.getElementById("header-color") as HTMLDivElement;
+const file = document.getElementById("file") as HTMLInputElement;
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+const ctx = canvas.getContext("2d", {
+  willReadFrequently: true,
+}) as CanvasRenderingContext2D;
 const zoom = document.getElementById("zoom") as HTMLDivElement;
 const zoomColor = document.getElementById("zoom-color") as HTMLDivElement;
 const zoomSize = 160;
@@ -13,6 +16,7 @@ let dropping = false;
 let hovering = false;
 let mouseX = 0;
 let mouseY = 0;
+let image = new Image();
 
 const update = () => {
   if (dropping) {
@@ -70,17 +74,37 @@ canvas.addEventListener("click", () => {
   }
 });
 
-const image = new Image();
-image.addEventListener("error", () => {
-  alert("Image load error");
+const loadImage = (url: string) => {
+  image = new Image();
+  image.addEventListener("error", () => {
+    alert("Image load error");
+  });
+  image.addEventListener("load", () => {
+    canvas.width = image.width;
+    canvas.height = image.height;
+    canvas.style.aspectRatio = String(image.width / image.height);
+    ctx.drawImage(image, 0, 0);
+    loaded = true;
+    update();
+  });
+  image.src = url;
+};
+
+file.addEventListener("change", () => {
+  if (file.files?.length === 1) {
+    loaded = false;
+    update();
+    const fileReader = new FileReader();
+    fileReader.addEventListener("error", () => {
+      alert("failed to read file");
+    });
+    fileReader.addEventListener("load", () => {
+      loadImage(fileReader.result as string);
+    });
+    fileReader.readAsDataURL(file.files[0]);
+  }
 });
-image.addEventListener("load", () => {
-  canvas.width = image.width;
-  canvas.height = image.height;
-  canvas.style.aspectRatio = String(image.width / image.height);
-  ctx.drawImage(image, 0, 0);
-  loaded = true;
-  update();
-});
-image.src =
-  "/1920x1080-4598441-beach-water-pier-tropical-sky-sea-clouds-island-palm-trees.jpg";
+
+loadImage(
+  "/1920x1080-4598441-beach-water-pier-tropical-sky-sea-clouds-island-palm-trees.jpg",
+);
